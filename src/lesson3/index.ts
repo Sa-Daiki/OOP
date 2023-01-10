104f87b1ed5900e1655e45fb19ca09a0a377baeb
Sa-Daiki <sato-daiki@evolu.tokyo>
2023-01-10 09:36:00 +0900

    ✨ create class

---
+++ src/lesson3/index.ts
@@ -0,0 +1,12 @@
+export class Player {
+  static STONE: number = 0;
+  static SCISSORS: number = 1;
+  static PAPER: number = 2;
+
+  private name = "";
+  private winCount = 0;
+
+  showHand() {}
+  notifyResult() {}
+  getWinCount() {}
+}
