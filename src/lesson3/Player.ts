a39cd9ef82a79b0b00272d37e54d56a63aeadeeb
Sa-Daiki <sato-daiki@evolu.tokyo>
2023-01-15 17:04:37 +0900

    👔 extract tactics method

--- src/lesson3/Player.ts
+++ src/lesson3/Player.ts
@@ -1,13 +1,20 @@
+import { Tactics } from "./tactics";
+
 export class Player {
   private name: string;
   private winCount = 0;
+  private tactics?: Tactics;
 
   constructor(name: string) {
     this.name = name;
   }
 
+  setTactics(tactics: Tactics) {
+    this.tactics = tactics;
+  }
+
   showHand() {
-    return Math.floor(Math.random() * 3);
+    if (this.tactics) return this.tactics.readTactics();
   }
   notifyResult() {
     this.winCount += 1;
